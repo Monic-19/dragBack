87eac97b57c132d5f2b37b42e06ca43fafd640a7
monic vyas <118365854+Monic-19@users.noreply.github.com>
2024-10-27 17:02:27 +0530

    code edited

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use('/uploads', express.static('uploads'));
 app.use(userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('WELCOME');
+})
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
