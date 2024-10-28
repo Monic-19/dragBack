9a7f40ebf313f1d59468b173d529cf370e57055b
monic vyas <118365854+Monic-19@users.noreply.github.com>
2024-10-28 13:10:40 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const multer = require('multer');
 const { addUser, getPaginatedUsers } = require('../controller/userController');
 
-
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+      cb(null, uniqueSuffix);
+    }
+  });
+const upload = multer({storage});
 
 router.post('/api/users', upload.single('image'), addUser);
 router.get('/api/users', getPaginatedUsers);
